c8e9a3de46773dff5ca5c340c23e0572e3ff8460
Edivan de Castro <edivandecastro@gmail.com>
2023-03-12 07:06:28 -0300

    Implementado action para criar um Country

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::CountriesController < ApplicationController
     countries = Api::V1::Country.all
     render json: countries, root: :countries
   end
+
+  def create
+    country = Api::V1::Country.new(country_params)
+    country.save
+    render json: country, root: :country
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:id, :name, :locale, :acronym,)
+  end
 end
